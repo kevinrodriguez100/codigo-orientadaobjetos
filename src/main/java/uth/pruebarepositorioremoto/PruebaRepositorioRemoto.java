84865cd17c293e47c8cfd2445e3b2b9b51130e42
kevinrodriguez100 <150084289+kevinrodriguez100@users.noreply.github.com>
2023-11-06 16:24:20 -0600

    primer commit para repositorio remoto

---
+++ src/main/java/uth/pruebarepositorioremoto/PruebaRepositorioRemoto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uth.pruebarepositorioremoto;
+
+/**
+ *
+ * @author ksalo
+ */
+public class PruebaRepositorioRemoto {
+
+    public static void main(String[] args) {
+        System.out.println("Vamos a guadar el codigo en github");
+    }
+}
